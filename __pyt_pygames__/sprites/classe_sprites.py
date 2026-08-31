c4223848ecbf074d3f30467955f5e21b86ceb7fa
HillaryMoraes <hillarysmoraes2609@gmail.com>
2026-08-31 16:53:18 -0300

    Não interessa

--- __pyt_pygames__/sprites/classe_sprites.py
+++ __pyt_pygames__/sprites/classe_sprites.py
@@ -1,12 +1,18 @@
 import pygame
+from pathlib import Path
+PASTA_RAIZ = Path(__file__).resolve().parent.parent
+PASTA_IMAGENS = PASTA_RAIZ / "movimentacao_diana"
 class Frames(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites=[]
-        self.image_a=pygame.image.load("carinha_sprites/sprite_0.png")
-        self.image_b=pygame.image.load("carinha_sprites/sprite_1.png")
+        self.image_a=pygame.image.load(str("PASTA_IMAGENS/Andar 1.jpeg"))
+        self.image_b=pygame.image.load(str("PASTA_IMAGENS/Andar 2.jpeg"))
+        self.image_c=pygame.image.load(str("PASTA_IMAGENS/Andar 3.jpeg"))
+
         self.sprites.append(self.image_a)
         self.sprites.append(self.image_b)
+        self.sprites.append(self.image_c)
         self.indice_atual=0
         self.image=self.sprites[self.indice_atual]
         self.image=pygame.transform.scale(self.image,(300,360)) #Redimensionar
@@ -20,9 +26,9 @@ class Frames(pygame.sprite.Sprite):
         self.image=self.sprites[int(self.indice_atual)]
         self.image=pygame.transform.scale(self.image,(300,360)) #Redimensionar
  
-todas_as_sprites=pygame.sprite.Group()
-carinha_deformado=Frames()
-todas_as_sprites.add(carinha_deformado)
+andar=pygame.sprite.Group()
+Diana=Frames()
+andar.add(Diana)
 
 
 
--- __pyt_pygames__/sprites/teste.py
+++ __pyt_pygames__/sprites/teste.py
@@ -1,17 +1,18 @@
 import pygame
-from __pyt_pygame__.sprites.classe_sprites import*
+from classe_sprites import*
 from pygame.locals import*
 pygame.init()
 clock=pygame.time.Clock()
 tela=pygame.display.set_mode((800,600))
-pygame.display.set_caption("Teste de horrores")
+pygame.display.set_caption("Movimentação")
 rodando=True
 while rodando==True:
     tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando=False
-    todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
+    andar.draw(tela)
+    andar.update()
     clock.tick(20)
     pygame.display.flip()
+    
